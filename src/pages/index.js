bfc0571eed46abe26881910ec6bb06b539c6863e
nihakue <walden.gabrielw@gmail.com>
2018-08-02 22:24:58 +0100

    Migrate to gatsby v2 beta

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import Link from 'gatsby-link'
+
+import Layout from '../components/layout'
 
 const IndexPage = () => (
-  <div>
+  <Layout>
     <h1>Under Construction</h1>
-    <p>Sorry for the inconvenience. The website will be up again soon</p>
-    <p>In the meantime, please go to my <a href="https://www.facebook.com/planticrub/"> facebook page</a></p>
-  </div>
+      <p>Sorry for the inconvenience. The website will be up again soon</p>
+      <p>
+        In the meantime, please go to my{' '}
+        <a href="https://www.facebook.com/planticrub/"> facebook page</a>
+      </p>
+  </Layout>
 )
 
 export default IndexPage
