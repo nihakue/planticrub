809f992f593289825e74836c1408d94a3563f268
nihakue <walden.gabrielw@gmail.com>
2018-08-02 20:57:15 +0100

    Initial gatsby setup

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Planticrub Garden Design',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Under Construction</h1>
+    <p>Please excuse the dust, things should be back to normal soon.</p>
+    <p>In the meantime, please go to my <a href="https://www.facebook.com/planticrub/"> facebook page</a></p>
+  </div>
+)
+
+export default IndexPage
