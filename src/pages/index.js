2078f7fd9c4ea0e350608b017187e7fc0786ffc1
Gabriel West <walden.gabrielw@gmail.com>
2018-08-02 21:32:36 +0100

    Update working on front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Under Construction</h1>
-    <p>Please excuse the dust, things should be back to normal soon.</p>
+    <p>Sorry for the inconvenience. The website will be up again soon</p>
     <p>In the meantime, please go to my <a href="https://www.facebook.com/planticrub/"> facebook page</a></p>
   </div>
 )
