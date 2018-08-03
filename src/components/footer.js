f79fce0b1c0e8bd66c2e52284f2c495a6e17fe9b
nihakue <walden.gabrielw@gmail.com>
2018-08-04 00:13:06 +0100

    add simple footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,49 @@
 import React from 'react'
 
+import contact from '../data/contact.json'
+
 import './footer.css'
 
+function ContactDetail({ field, value }) {
+  switch (field) {
+    case 'email':
+      return (
+        <a href={`mailto:${value}`} target="_top">
+          {value}
+        </a>
+      )
+    case 'phone':
+      return (
+        <a href={`tel:${value}`} target="_top">
+          {value}
+        </a>
+      )
+    default:
+      return <span>{value}</span>
+  }
+}
+
+function ContactDetails() {
+  return (
+    <ul className="contact">
+      {Object.keys(contact).map(key => (
+        <li key={key}>
+          <ContactDetail field={key} value={contact[key]} />
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+function Copyright() {
+  return <div className="copyright">© 2018 Planticrub</div>
+}
+
 export default function Footer() {
-  return <div className="footer"> Contact details and copyright</div>
+  return (
+    <div className="footer">
+      <ContactDetails />
+      <Copyright />
+    </div>
+  )
 }
