fad999b141ce6ca50731cc0414b21fc16ed15eba
nihakue <walden.gabrielw@gmail.com>
2018-08-04 17:32:12 +0100

    improve md image perf

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,8 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590,
+              quality: 75,
+              withWebp: true,
             },
           },
         ],
