43c3e58d12727bf4f91a7a7ca8bde5c4949bde1f
nihakue <walden.gabrielw@gmail.com>
2019-05-02 19:41:43 +0100

    migrate to useStaticQuery

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,45 +1,39 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 import Img from 'gatsby-image'
 
 import './gallery.css'
 
 export default function Gallery() {
-  return (
-    <StaticQuery
-      query={graphql`
-        {
-          gallery: allFile(filter: { sourceInstanceName: { eq: "gallery" } }) {
-            edges {
-              node {
-                childImageSharp {
-                  fluid(maxWidth: 250, maxHeight: 250, quality: 100) {
-                    originalName
-                    ...GatsbyImageSharpFluid
-                  }
-                }
+  const data = useStaticQuery(graphql`
+    {
+      gallery: allFile(filter: { sourceInstanceName: { eq: "gallery" } }) {
+        edges {
+          node {
+            childImageSharp {
+              fluid(maxWidth: 250, maxHeight: 250, quality: 100) {
+                originalName
+                ...GatsbyImageSharpFluid
               }
             }
           }
         }
-      `}
-      render={data => {
-        const { edges } = data.gallery
+      }
+    }
+  `)
+  const { edges } = data.gallery
+  return (
+    <div className="gallery">
+      {edges.map(edge => {
+        const { fluid } = edge.node.childImageSharp
         return (
-          <div className="gallery">
-            {edges.map(edge => {
-              const { fluid } = edge.node.childImageSharp
-              return (
-                <Img
-                  key={fluid.originalName}
-                  alt={fluid.originalName}
-                  fluid={fluid}
-                />
-              )
-            })}
-          </div>
+          <Img
+            key={fluid.originalName}
+            alt={fluid.originalName}
+            fluid={fluid}
+          />
         )
-      }}
-    />
+      })}
+    </div>
   )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, StaticQuery, graphql } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import Helmet from 'react-helmet'
 import Img from 'gatsby-image'
 
@@ -7,63 +7,59 @@ import Nav from './nav'
 
 import './header.css'
 
-const Header = () => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        logo: file(relativePath: { eq: "logo.png" }) {
-          childImageSharp {
-            fixed(width: 50) {
-              ...GatsbyImageSharpFixed
-            }
-          }
-        }
-        site {
-          siteMetadata {
-            title
+const Header = () => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      logo: file(relativePath: { eq: "logo.png" }) {
+        childImageSharp {
+          fixed(width: 50) {
+            ...GatsbyImageSharpFixed
           }
         }
       }
-    `}
-    render={data => {
-      const { title } = data.site.siteMetadata
-      const { fixed } = data.logo.childImageSharp
-      return (
-        <>
-          <Helmet
-            title={title}
-            meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
-            ]}
-          />
-          <div className="header">
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+  const { title } = data.site.siteMetadata
+  const { fixed } = data.logo.childImageSharp
+  return (
+    <>
+      <Helmet
+        title={title}
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      />
+      <div className="header">
+        <Link
+          to="/"
+          style={{
+            textDecoration: 'none',
+          }}
+        >
+          <Img fixed={fixed} alt="logo" />
+        </Link>
+        <div className="logo">
+          <h1>
             <Link
               to="/"
               style={{
                 textDecoration: 'none',
               }}
             >
-              <Img fixed={fixed} alt="logo" />
+              {title}
             </Link>
-            <div className="logo">
-              <h1>
-                <Link
-                  to="/"
-                  style={{
-                    textDecoration: 'none',
-                  }}
-                >
-                  {title}
-                </Link>
-              </h1>
-            </div>
-            <Nav />
-          </div>
-        </>
-      )
-    }}
-  />
-)
+          </h1>
+        </div>
+        <Nav />
+      </div>
+    </>
+  )
+}
 
 export default Header
